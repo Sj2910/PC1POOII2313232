cfb451d2f5cfe0663512d30ce7efc73268c10386
Sj2910 <142682922+Sj2910@users.noreply.github.com>
2023-09-23 12:40:28 -0500

    Add files via upload

---
+++ PracticaCalificadaI.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.calificada.i;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class PracticaCalificadaI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner scanner = new Scanner (System.in);
+        int cantPuntos;
+        int suma;
+        
+        System.out.println("Ingrese la cantidad de puntos que su equipo ha obtenido en el juego");
+        cantPuntos=scanner.nextInt();
+        
+        while(cantPuntos!=-1){
+            cantPuntos++;
+        System.out.println("Ingrese la cantidad de puntos que su equipo ha obtenido en el juego");
+        
+        
+        if(cantPuntos>=0){
+        suma=cantPuntos+1;
+        }
+        
+        
+        
+        
+    
+        
+        
+        
+        }
+        
+    }
+    
+}
